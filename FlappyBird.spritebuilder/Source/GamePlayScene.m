a8c916421768c53f282a86f4cfb57fc020e8c421
SalGs <S_Granados@Sauls-MacBook-Air.local>
2015-05-26 03:57:30 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,13 +10,21 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsnode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
     
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f) {
+    }
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
+
 
 // put new methods here
 -(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
